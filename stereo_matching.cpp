2e0f34b3ad7807ba62ce7237bf686107f2e32ee5
HsuanYuYao <sherry641561@gmail.com>
2023-09-11 23:09:14 +0200

    Add CMakeLists.txt

---
+++ stereo_matching.cpp
@@ -0,0 +1,10 @@
+#include "stereo_matching.h"
+#include <opencv2/core/mat.hpp>
+
+cv::Mat NaiveSearch::operator() (cv::Mat img1, cv::Mat img2) {
+    return 0;
+}
+
+cv::Mat PatchMatch::operator() (cv::Mat img1, cv::Mat img2) {
+    return 0;
+}
